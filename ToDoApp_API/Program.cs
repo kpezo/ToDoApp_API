5a56f2c1ccb793f1319f9499738d25ef505e1e3d
kpezo <kresimir.pezo@gmail.com>
2022-11-16 12:53:46 +0100

    Finished jwt token generation

--- ToDoApp_API/Program.cs
+++ ToDoApp_API/Program.cs
@@ -2,24 +2,30 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 using ToDoApp_API.DatabaseContext;
+using ToDoApp_API.Repository.Interfaces;
+using ToDoApp_API.Repository;
+using ToDoApp_API.Services.Authentication;
+using ToDoApp_API.Services.Authentication.Interfaces;
+using ToDoApp_API.Services.Common.Interfaces;
+using ToDoApp_API.Services.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(JwtSettings.SectionName));
+
+builder.Services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddDbContext<AppDbContext>
                 (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-//builder.Services.AddDbContext<AppDbContext>
-//(options => options.UseSqlServer(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
 
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-//});
 
-//builder.Services.AddScoped<ISessionService, SessionService>();
-//builder.Services.AddScoped<ISessionRepository, SessionRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
